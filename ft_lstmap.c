1adb29fb69e5a54865b45831eddf2137d25ab971
Ana Medina Burgos <anamedin@car1s5.42barcelona.com>
2024-05-04 18:13:44 +0200

    repasolistas

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,20 +6,14 @@
 /*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:04:23 by anamedin          #+#    #+#             */
-/*   Updated: 2024/05/03 22:01:51 by anamedin         ###   ########.fr       */
+/*   Updated: 2024/05/04 03:41:24 by anamedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-
-
-
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
-
 
 //lst: un puntero a un nodo
 // f:  la dirección de un puntero a una función usada
@@ -38,72 +32,51 @@ typedef struct s_list
 	// INTERGER * NUM
 	// GUARDARLO EN NEW_NODE
 	// return new_node
-	//
 
-void	*multiply(void *content)
+/*void    *multiply(void *content)
 {
-	t_list	*new_node = NULL;
-	int		result;
-	int		num;
-	char	*str_num;
+    int     result;
+	char	*str;
 
-
-	//str_num = (char *)content
-	result = 2;
-	str_num = (char *)new_node->content;
-	num = atoi(str_num);
-	result = result * num;
-	*(int *)new_node->content = result;
-	return ((void *)new_node);
-}
-
-
-/*t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-    t_list *new_lst = NULL;
-    t_list *curr = lst;
-    t_list *new_node;
-
-    while (curr)
-    {
-        new_node = ft_lstnew(f(curr->content));
-        if (!new_node)
-        {
-            ft_lstclear(&new_lst, del);
-            return (NULL);
-        }
-        ft_lstadd_back(&new_lst, new_node);
-        curr = curr->next;
-    }
-    return (new_lst);
+	str = NULL;
+	result = 2 * atoi((char *)content);
+	str = itoa(result);
+    return ((void *)str);
 }*/
 
-
-
-t_list	ft_lstmap(t_list *lst, void*(*f)(void *), void (*del)(void *))
+void	*f(void *content)
 {
-	t_list *lst = (t_list *) malloc(sizeof(t_list));
-	t_list *tmp = lst;
-	t_list *new_node = (t_list *) malloc(sizeof(t_list));
-
-	while (tmp)
-	{
-		new_node = ft_lstnew(f(tmp->content));
-		if (!new_node)
-		{
-			while (new_lst)
-			{
-				t_list *next = new_lst->next;
-				del(new_lst->content);
-				free(new_lst);
-				new_lst = next;
-			}
-		}
-		ft_lstadd_back(&new_lst, new_node);
-		tmp = tmp->next;
-	}
-	return (*new_lst);
+    int *original = (int *)content;
+    int *duplicated = (int *)malloc(sizeof(int));
+    if (!duplicated)
+        return NULL;
+    *duplicated = *original * 2;
+    return (duplicated);
 }
+
+
+
+t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list *new_lst = NULL;
+    t_list *tmp = lst;
+
+    while (tmp)
+    {
+        t_list *new_node = ft_lstnew(f(tmp->content));
+        if (!new_node)
+        {
+            ft_free(&new_lst);
+            return NULL;
+        }
+        ft_lstadd_back(&new_lst, new_node);
+        tmp = tmp->next;
+    }
+    return new_lst;
+}
+
+
+
 
 t_list *ft_lstnew (void *content)
 {
@@ -133,18 +106,6 @@ void ft_lstadd_back(t_list **lst, t_list *new)
 	tmp->next = new;
 }
 
-/*void ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list *tmp;
-	while (*lst)
-	{
-		tmp = (*lst)->next;
-		del((*lst)->content);
-		free(*lst);
-		*lst = tmp;
-	}
-}*/
-
 void ft_free(t_list **lst)
 {
 	t_list *tmp;
@@ -158,46 +119,51 @@ void ft_free(t_list **lst)
 	}
 }
 
-int main (void)
+int main(void)
 {
-	t_list *lista = ft_lstnew(strdup(""));
-	t_list *node1 = ft_lstnew(strdup("10"));
-	t_list *node2 = ft_lstnew(strdup("25"));
-	t_list *node3 = ft_lstnew(strdup("40"));
-	t_list *node4 = ft_lstnew(strdup("75"));
+    t_list *lista = ft_lstnew(strdup(""));
+    t_list *node1 = ft_lstnew(strdup("10"));
+    t_list *node2 = ft_lstnew(strdup("25"));
+    t_list *node3 = ft_lstnew(strdup("40"));
+    t_list *node4 = ft_lstnew(strdup("75"));
 
-   	 //creamos lista
-	lista->next = node1;
-	node1->next = node2;
-	node2->next = node3;
-	node3->next = node4;
-	
-	//RECORRER LISTA DESPUES:
-	t_list	*tmp = lista;
-	int i;
-	i = 0;
+    // Creamos lista
+    lista->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+
+    // Recorremos lista antes de la función
+    t_list *tmp = lista;
+    int i = 0;
     while (tmp != NULL)
-	{
-		printf("nodo[%d]: %s\n", i, tmp->content);
-		tmp = tmp->next;
-		i++;
+    {
+        printf("nodo[%d]: %s\n", i, (char *)tmp->content);
+        tmp = tmp->next;
+        i++;
     }
 
+    // Llamamos a la función
+    t_list *new_lst = ft_lstmap(lista, &multiply, ft_free);
+    if (!new_lst)
+    {
+        printf("Error al mapear la lista.\n");
+        return 1;
+    }
 
-	t_list *new_nodo = (t_list *) malloc(sizeof(t_list));
-	//LLAMAR FUNCION:
-	ft_lstmap(new_list, &multiply ,del);
-	t_list *tmp2 = new_lst;
-	i = 0;
-	while (tmp2 != NULL)
-	{
-		printf("nuevo nodo[%d]: %s\n", i , tmp2->content);
-		tmp2 = tmp2->next;
-		i++;
-	}
+    // Recorremos la nueva lista
+    tmp = new_lst;
+    i = 0;
+    while (tmp != NULL)
+    {
+        printf("nuevo nodo[%d]: %d\n", i, *(int *)(tmp->content));
+        tmp = tmp->next;
+        i++;
+    }
 
-	//RECORRER DESPUES DE FUNCION:
-	
-	ft_free(&lista);
-    return (0);
+    // Liberamos memoria
+    ft_free(&lista);
+    ft_free(&new_lst);
+
+    return 0;
 }
--- one.c
+++ one.c
@@ -6,10 +6,86 @@
 /*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 19:22:59 by anamedin          #+#    #+#             */
-/*   Updated: 2024/05/03 14:21:48 by anamedin         ###   ########.fr       */
+/*   Updated: 2024/05/03 22:27:25 by anamedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+void    *multiply(void *content)
+{
+    int result = 2 * atoi((char *)content);
+
+    t_list *new_node = ft_lstnew(&result, sizeof(int));
+    if (!new_node)
+        return NULL;
+
+    return (void *)new_node;
+}
+t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list *new_lst = NULL;
+    t_list *tmp = lst;
+
+    while (tmp)
+    {
+        t_list *new_node = (t_list *)f(tmp->content);
+        if (!new_node)
+        {
+            ft_free(&new_lst);
+            return NULL;
+        }
+        ft_lstadd_back(&new_lst, new_node);
+        tmp = tmp->next;
+    }
+    return new_lst;
+}
+int main(void)
+{
+    t_list *lista = ft_lstnew(strdup(""));
+    t_list *node1 = ft_lstnew(strdup("10"));
+    t_list *node2 = ft_lstnew(strdup("25"));
+    t_list *node3 = ft_lstnew(strdup("40"));
+    t_list *node4 = ft_lstnew(strdup("75"));
+
+    // Creamos lista
+    lista->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+
+    // Recorremos lista antes de la función
+    t_list *tmp = lista;
+    int i = 0;
+    while (tmp != NULL)
+    {
+        printf("nodo[%d]: %s\n", i, (char *)tmp->content);
+        tmp = tmp->next;
+        i++;
+    }
+
+    // Llamamos a la función
+    t_list *new_lst = ft_lstmap(lista, &multiply, ft_free);
+    if (!new_lst)
+    {
+        printf("Error al mapear la lista.\n");
+        return 1;
+    }
+
+    // Recorremos la nueva lista
+    tmp = new_lst;
+    i = 0;
+    while (tmp != NULL)
+    {
+        printf("nuevo nodo[%d]: %d\n", i, *(int *)(tmp->content));
+        tmp = tmp->next;
+        i++;
+    }
+
+    // Liberamos memoria
+    ft_free(&lista);
+    ft_free(&new_lst);
+
+    return 0;
+}
 
